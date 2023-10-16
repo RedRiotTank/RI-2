6656a0611f793bc0191931cda539b5765835e0d0
Alberto Plaza <albertoplazamontesdm@gmail.com>
2023-10-16 20:15:00 +0200

    add: neutral, fix: main

--- src/main/java/ri/core/Utils.java
+++ src/main/java/ri/core/Utils.java
@@ -11,8 +11,6 @@ public class Utils {
         return filename.substring(0, filename.lastIndexOf('.'));
     }
 
-
-
     public static List<Map.Entry<String, Integer>> orderFrequencyMap(Map<String, Integer> frequencyMap){
         List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(frequencyMap.entrySet());
         sortedEntries.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
@@ -24,5 +22,20 @@ public class Utils {
         return name.endsWith(".html") || name.endsWith(".htm");
     }
 
+    public static void printHelp() {
+        System.out.println("Usage: java -jar RI2.jar <directory> [option]  [filter]");
+
+        System.out.println();
+
+        System.out.println("Options:");
+
+        System.out.println("    -d: Print the table of the files in the directory");
+        System.out.println("    -l: Print the table of the links in the directory");
+        System.out.println("    -t: Generate the word count of the files in the directory in CSV format");
+        System.out.println("    -allAnalyzers: Generate the word count of the files in the directory in CSV format for all analyzers");
+        System.out.println("    -tokenFilter: ? Important: needs filter option");
+        System.out.println("    -checkMood: Generate the Mood word count of the files in the directory in CSV format");
+
+    }
 
 }
